ad017cacd6be63286bfb966c7434f2447e050cc4
puneetkathar1 <73285338+puneetkathar1@users.noreply.github.com>
2021-06-15 13:17:22 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     REACT_APP_GOOGLE_MAP_API_KEY:
       'https://maps.googleapis.com/maps/api/js?v=3.exp&key=YOUR_GOOGLE_MAP_API_KEY&libraries=geometry,drawing,places',
 
-    SERVER_API: `http://localhost:3001`,
+    SERVER_API: `https://test2-zeta-steel.vercel.app`,
   },
   webpack: (config, { isServer }) => {
     // HOTFIX: https://github.com/webpack-contrib/mini-css-extract-plugin/issues/250
